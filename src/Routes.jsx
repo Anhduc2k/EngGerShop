04de3139180173032752c2dd8e154e38c7c70dbf
AnhDuc2k <khungkhip@gmail.com>
2021-10-17 16:00:01 +0700

    UnauthenticatedGuard & AuthenticatedGuard

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { path } from './constants/path'
+import AuthenticatedGuard from './guards/AuthenticatedGuard'
+import UnAuthenticatedGuard from './guards/UnAuthenticatedGuard'
 import MainLayout from './layouts/MainLayout/MainLayout'
 import RegisterLayout from './layouts/RegisterLayout'
 import Login from './pages/Auth/Login/Login'
 import Register from './pages/Auth/Register/Register'
 import Home from './pages/Home/Home'
 import NotFound from './pages/NotFound/NotFound'
+import User from './pages/User/User'
 
 export default function Routes() {
   return (
@@ -17,14 +20,25 @@ export default function Routes() {
         </MainLayout>
       </Route>
       <Route path={path.register}>
-        <RegisterLayout title="Đăng kí">
-          <Register />
-        </RegisterLayout>
+        <UnAuthenticatedGuard>
+          <RegisterLayout title="Đăng kí">
+            <Register />
+          </RegisterLayout>
+        </UnAuthenticatedGuard>
       </Route>
       <Route path={path.login}>
-        <RegisterLayout title="Đăng nhập">
-          <Login />
-        </RegisterLayout>
+        <UnAuthenticatedGuard>
+          <RegisterLayout title="Đăng nhập">
+            <Login />
+          </RegisterLayout>
+        </UnAuthenticatedGuard>
+      </Route>
+      <Route path={path.user}>
+        <AuthenticatedGuard>
+          <MainLayout>
+            <User />
+          </MainLayout>
+        </AuthenticatedGuard>
       </Route>
       <Route path={path.notFound}>
         <NotFound />
