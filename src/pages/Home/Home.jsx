6d0521d86e3680986495dff9a2fd9a00a686e36e
AnhDuc2k <khungkhip@gmail.com>
2021-10-18 10:28:17 +0700

    make UI SortBar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FilterPanel from '../../components/FilterPanel/FilterPanel'
+import SearchItemResult from '../../components/SearchItemResult/SearchItemResult'
 import * as S from './home.style'
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
         <S.Side>
           <FilterPanel />
         </S.Side>
-        <S.Main></S.Main>
+        <S.Main>
+          <SearchItemResult />
+        </S.Main>
       </S.Container>
     </div>
   )
